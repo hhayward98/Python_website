274ee14960ba54c06ecb4c22102246d02d8ea748
unknown <arkidenOT@outlook.com>
2021-09-29 10:32:09 -0600

    changed client/server to del json files when exiting game

--- httpServer/client.py
+++ httpServer/client.py
@@ -1,13 +1,12 @@
-
 import os
-
 import requests
 import json
-
 
 FORMAT = "utf-8"
 PORT = 8080
 SERVER =  "127.0.0.1" #Gets local ip
+
+
 
 
 
@@ -27,41 +26,56 @@ def main():
     while exitCommand :
 
 
-        choice = input("\n1 -- Game Score\n2 -- Throw a Play\n3 -- Play Result\n4 -- Reset Game\n5 -- Quit\n")
+        # choice = input("\n1 -- Game Score\n2 -- Throw a Play\n3 -- Play Result\n4 -- Reset Game\n5 -- Quit\n")
+        print("Enter an option to continue!\n")
+        choice_game = input("\n1 -- Play game\n2 -- Quit\n")
 
-        if (int(choice) == 1):
-            r = requests.get('http://localhost:8000/gamescore')
-            print(f"\n {r.text} \n")
-        elif (int(choice) == 2):
-            throw = input("\nRock, Paper or Scissors? ")
+        if (int(choice_game) == 1):
+            while endGame :
 
-            if throw.lower() == "rock" or throw.lower() == "paper" or throw.lower() == "scissors":
-                if (throw.lower() == "rock"):
-                    throw = str(0)
-                elif(throw.lower() == "paper"):
-                    throw = str(-1)
-                elif(throw.lower() == "scissors"):
-                    throw = str(1)
+                choice = input("\n1 -- Game Score\n2 -- Throw a Play\n3 -- Reset Game\n4 -- Quit\n")
 
-                payload = {"throw" : throw}
-                r = requests.post(f'http://localhost:8000/throw/{NAME}', json=json.dumps(payload))
-                if (r.status_code == requests.codes.conflict):
-                    print(f"\nStatus Code: {r.status_code} -- You played a duplicate throw.\n")
+                if (int(choice) == 1):
+                    r = requests.get('http://localhost:8000/gamescore')
+                    print(f"\n {r.text} \n")
+                elif (int(choice) == 2):
+                    throw = input("\nRock, Paper or Scissors? ")
 
-                else:
+                    if throw.lower() == "rock" or throw.lower() == "paper" or throw.lower() == "scissors":
+                        if (throw.lower() == "rock"):
+                            throw = str(0)
+                        elif(throw.lower() == "paper"):
+                            throw = str(-1)
+                        elif(throw.lower() == "scissors"):
+                            throw = str(1)
+
+                        payload = {"throw" : throw}
+                        r = requests.post(f'http://localhost:8000/throw/{NAME}', json=json.dumps(payload))
+                        if (r.status_code == requests.codes.conflict):
+                            print(f"\nStatus Code: {r.status_code} -- You played a duplicate throw.\n")
+                        else:
+                            print(f"\nStatus Code: {r.status_code} -- {r.text} \n")
+                    else:
+                        print('\nPlease enter Rock, Paper or Scissors.')
+
+                    print("\nRuning game")
+                    print("\nDetermining results\n")
+
+                    r = requests.get('http://localhost:8000/playresult')
+                    print(f"\nStatus Code: {r.status_code} --  {r.text} \n")
+
+                elif (int(choice) == 3):
+                    payload = {"reset" : 'True'}
+                    r = requests.post(f'http://localhost:8000/reset/{NAME}', json=json.dumps(payload))
                     print(f"\nStatus Code: {r.status_code} -- {r.text} \n")
-            else:
-                print('\nPlease enter Rock, Paper or Scissors.')
 
-        elif (int(choice) == 3):
-
-            r = requests.get('http://localhost:8000/playresult')
-            print(f"\n {r.text} \n")
-        elif (int(choice) == 4):
-            payload = {"reset" : 'True'}
-            r = requests.post(f'http://localhost:8000/reset/{NAME}', json=json.dumps(payload))
-            print(f"\nStatus Code: {r.status_code} -- {r.text} \n")
-        elif (int(choice) == 5): 
+                elif (int(choice) == 4):
+                    print("\nExiting Game")
+                    endGame = False
+                else:
+                    print("\nPlease enter an option from the menu.\n")
+        elif (int(choice) == 2): 
+            r = requests.post(f'http://localhost:8000/exit/{NAME}', json=json.dumps(payload))
             print("\nThanks for playing!")
             exitCommand = False
         else:
--- httpServer/server.py
+++ httpServer/server.py
@@ -6,37 +6,37 @@ import socket
 
 FORMAT = "utf-8"
 PORT = 8080
-SERVER = socket.gethostbyname(socket.gethostname()) #Gets local ip
+SERVER = socket.gethostbyname(socket.gethostname())  # Gets local ip
+
+
 
 
 
 def startGame():
     print("Game has started...")
 
+
 class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
     """A custom HTTP Request Handler based on SimpleHTTPRequestHandler"""
 
-    
     server_version = "My_HTTP_Server/"
     path_prefix = "www"
-    
+
     def __init__(self, *args, directory=None, **kwargs):
         if directory is None:
             directory = os.getcwd()  # start file path with the current directory
         self.directory = directory  # or with the directory passed in the argument
         super().__init__(*args, **kwargs)  # initialize the base handler
 
-
-
-    def do_GET(self):        
+    def do_GET(self):
         self.path = self.path_prefix + self.path
 
         path = self.path.split("/")
-        
+
         if (path[1] == 'gamescore'):
             try:
                 response = self.getScore()
-            
+
                 self.send_response(HTTPStatus.OK)
                 self.end_headers()
                 self.wfile.write((response).encode())
@@ -46,15 +46,14 @@ class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
         elif (path[1] == 'playresult'):
             try:
                 response = self.playResult()
-            
+
                 self.send_response(HTTPStatus.OK)
                 self.end_headers()
                 self.wfile.write((response).encode())
-    
+
             except OSError:
                 self.send_response(HTTPStatus.NOT_FOUND)
                 self.end_headers()
-
 
     def do_POST(self):
 
@@ -65,7 +64,7 @@ class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
 
         path = self.path.split("/")
 
-        if (path[1] == 'new' ):
+        if (path[1] == 'new'):
             joinStatus = self.setPlayers(content, self.client_address)
             # here we response to wait for other player or start game
             if (joinStatus):
@@ -73,21 +72,21 @@ class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
                 self.end_headers()
                 self.wfile.write(("You have joined the game!").encode())
 
-                # startGame()
+                startGame()
             else:
                 self.send_response(HTTPStatus.OK)
                 self.end_headers()
                 self.wfile.write(("Sorry Two Players have joined already").encode())
-            
+
 
         elif (path[1] == 'throw'):
             throwStatus = self.processThrow(content, path)
-            result = self.playResult()
+            # result = self.playResult()
 
             if (throwStatus != 0):
                 self.send_response(HTTPStatus.OK)
                 self.end_headers()
-                self.wfile.write((f"{throwStatus}\n{result}").encode())
+                self.wfile.write((f"{throwStatus}\n").encode())
             else:
                 self.send_response(HTTPStatus.CONFLICT)
                 self.end_headers()
@@ -98,11 +97,16 @@ class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
             self.send_response(HTTPStatus.OK)
             self.end_headers()
             self.wfile.write((resetStatus).encode())
+        
+        elif (path[1] == 'exit'):
+            self.Exit_game(content, path)
+            self.send_response(HTTPStatus.OK)
+            self.end_headers()
 
 
     def setPlayers(self, content, client_address):
 
-        client = { "ip" : client_address[0],"port" : client_address[1], "score": "0", 'reset': "False", "throw": ""}
+        client = {"ip": client_address[0], "port": client_address[1], "score": "0", 'reset': "False", "throw": ""}
         client.update(content)
         print(client)
         if (os.path.exists("www/player1.json") and os.path.exists("www/player2.json")):
@@ -119,40 +123,41 @@ class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
             return True
 
     def processThrow(self, content, path):
-        pf1 = open("www/player1.json" , 'r+')
+        pf1 = open("www/player1.json", 'r+')
         playerData1 = json.load(pf1)
         if (os.path.exists("www/player2.json")):
-           pf2 = open("www/player2.json" , 'r+')
-           playerData2 = json.load(pf2)
+            pf2 = open("www/player2.json", 'r+')
+            playerData2 = json.load(pf2)
 
         if (playerData1['throw'] == content["throw"]):
             return 0
-        
+
         elif (playerData1['name'] == path[2]):
-                playerData1.update(content)
-                self.writeToFile(pf1, playerData1)
-                return "Throw was recorded."
+            playerData1.update(content)
+            self.writeToFile(pf1, playerData1)
+            return "Throw was recorded."
         elif (playerData2['throw'] == content["throw"]):
             return 0
         elif (playerData2['name'] == path[2]):
-                playerData2.update(content)
-                self.writeToFile(pf2, playerData2)
-                return "Throw was recorded."
+            playerData2.update(content)
+            self.writeToFile(pf2, playerData2)
+            return "Throw was recorded."
         else:
             return 0
 
-
     def reset(self, content, path):
-        pf1 = open("www/player1.json" , 'r+')
+        pf1 = open("www/player1.json", 'r+')
         playerData1 = json.load(pf1)
         if (os.path.exists("www/player2.json")):
-           pf2 = open("www/player2.json" , 'r+')
-           playerData2 = json.load(pf2)
+            pf2 = open("www/player2.json", 'r+')
+            playerData2 = json.load(pf2)
 
-        if ((playerData1["reset"] == "True" and playerData2["reset"] == "True") or (playerData1["reset"] == "True" and playerData2['name'] == path[2]) or (playerData2["reset"] == "True" and playerData1['name'] == path[2])):
+        if ((playerData1["reset"] == "True" and playerData2["reset"] == "True") or (
+                playerData1["reset"] == "True" and playerData2['name'] == path[2]) or (
+                playerData2["reset"] == "True" and playerData1['name'] == path[2])):
 
-            #os.remove("www/player2.json")
-            #os.remove("www/player1.json")
+            # os.remove("www/player2.json")
+            # os.remove("www/player1.json")
             playerData1["throw"] = ''
             playerData2["throw"] = ''
             playerData1["reset"] = 'False'
@@ -175,16 +180,30 @@ class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
             else:
                 return "You have already requested to reset."
 
-    def playResult(self):
-        if (os.path.exists("www/player1.json") and os.path.exists("www/player2.json")):
-            self.wfile.write(("Evaluating Result of game").encode())
-            startGame()
-            pf1 = open("www/player1.json" , 'r+')
+    def Exit_game(self, content, path):
+        if (os.path.exists("www/player1.json")):
+            pf1 = open("www/player1.json", 'r+')
             playerData1 = json.load(pf1)
-            pf2 = open("www/player2.json" , 'r+')
+        if (os.path.exists("www/player2.json")):
+            pf2 = open("www/player2.json", 'r+')
             playerData2 = json.load(pf2)
 
-            if(playerData1["throw"] != '' and playerData2["throw"] != ''):
+        if playerData1['name'] == path[2]:
+            os.remove("www/player1.json")
+            return 0
+        elif playerData2['name'] == path[2]:
+            os.remove("www/player2.json")
+            return 0
+
+    
+    def playResult(self):
+        if (os.path.exists("www/player1.json") and os.path.exists("www/player2.json")):
+            pf1 = open("www/player1.json", 'r+')
+            playerData1 = json.load(pf1)
+            pf2 = open("www/player2.json", 'r+')
+            playerData2 = json.load(pf2)
+
+            if (playerData1["throw"] != '' and playerData2["throw"] != ''):
                 throw1 = int(playerData1["throw"])
                 throw2 = int(playerData2["throw"])
                 score = self.compareThrows(throw1, throw2)
@@ -199,7 +218,7 @@ class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
                     playerData1.update({'score': str(scoreValue)})
                     self.writeToFile(pf1, playerData1)
                     self.writeToFile(pf2, playerData2)
-                    return f"Player {playerData1['name']} won that turn." 
+                    return f"Player {playerData1['name']} won that turn."
                 else:
                     scoreValue = int(playerData2["score"])
                     scoreValue += 1
@@ -208,21 +227,22 @@ class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
                     self.writeToFile(pf1, playerData1)
                     return f"Player {playerData2['name']} won that turn."
 
-            else: return "Still waiting on other player throw"
+            else:
+                return "Still waiting on other player throw"
         else:
             return "Still waiting on other player to join."
 
     def getScore(self):
         if (os.path.exists("www/player1.json") and os.path.exists("www/player2.json")):
-            pf1 = open("www/player1.json" , 'r+')
+            pf1 = open("www/player1.json", 'r+')
             playerData1 = json.load(pf1)
-            pf2 = open("www/player2.json" , 'r+')
+            pf2 = open("www/player2.json", 'r+')
             playerData2 = json.load(pf2)
             return f"Score is: {playerData1['name']} with {playerData1['score']} wins too {playerData2['name']} with {playerData2['score']} wins."
         else:
             return "Score issn't avalible until both players have joined."
 
-    def compareThrows(self, p1Throw, p2Throw):   
+    def compareThrows(self, p1Throw, p2Throw):
         tie = 2
         if (p1Throw != p2Throw):
             if (abs(p1Throw) == abs(p2Throw)):
@@ -230,7 +250,6 @@ class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
             else:
                 tie = min(p1Throw, p2Throw)
         return tie
-    
 
     def writeToFile(self, file, payload):
         file.seek(0)
@@ -245,6 +264,7 @@ def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
     httpd = server_class(server_address, handler_class)
     print(f"Running on port {PORT}...")
     httpd.serve_forever()
+
 
 if __name__ == "__main__":
     run(HTTPServer, MyHTTPRequestHandler)
