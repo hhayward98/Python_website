f9876c3df12c45477c3105c3688decd42f886081
HHayward98 <arkidenOT@outlook.com>
2022-06-28 12:03:38 -0600

    error handling

--- httpServer/client.py
+++ httpServer/client.py
@@ -8,8 +8,6 @@ SERVER =  "127.0.0.1" #Gets local ip
 
 
 
-
-
 NAME = ''
 
 
@@ -25,21 +23,43 @@ def main():
 
     while exitCommand :
 
+        Loop_int = 0
 
         # choice = input("\n1 -- Game Score\n2 -- Throw a Play\n3 -- Play Result\n4 -- Reset Game\n5 -- Quit\n")
         print("Enter an option to continue!\n")
-        choice_game = input("\n1 -- Play game\n2 -- Quit\n")
+
+        while Loop_int < 1:
+            Loop_int = 12 
+            choice_game = input("\n1 -- Play game\n2 -- Quit\n")
+            try:
+                choice_game = int(choice_game)
+            except ValueError as e:
+                Loop_int = 0
+                print("please enter a valid choice")
+
+        #check function
 
         if (int(choice_game) == 1):
             while endGame :
 
-                choice = input("\n1 -- Game Score\n2 -- Throw a Play\n3 -- Reset Game\n4 -- Quit\n")
+                L2 = 0 
+                while L2 < 1:
+                    L2 = 2
+                    choice = input("\n1 -- Game Score\n2 -- Throw a Play\n3 -- Reset Game\n4 -- Quit\n")
+                    try:
+                        choice = int(choice)
+                    except ValueError as e:
+                        L2 = 0
+                        print("please enter a valid choice")
+
 
                 if (int(choice) == 1):
                     r = requests.get('http://localhost:8000/gamescore')
                     print(f"\n {r.text} \n")
                 elif (int(choice) == 2):
+
                     throw = input("\nRock, Paper or Scissors? ")
+
 
                     if throw.lower() == "rock" or throw.lower() == "paper" or throw.lower() == "scissors":
                         if (throw.lower() == "rock"):
@@ -55,14 +75,28 @@ def main():
                             print(f"\nStatus Code: {r.status_code} -- You played a duplicate throw.\n")
                         else:
                             print(f"\nStatus Code: {r.status_code} -- {r.text} \n")
+
+                        print("\nRuning game")
+                        print("\nDetermining results\n")
+                        r = requests.get('http://localhost:8000/playresult')
+                        print(f"\nStatus Code: {r.status_code} --  {r.text} \n")
+
                     else:
-                        print('\nPlease enter Rock, Paper or Scissors.')
+                        print("____________________________________________")
+                        print('ERROR!!! your choice was Invalid, try again!')
+                        print("____________________________________________")
 
-                    print("\nRuning game")
-                    print("\nDetermining results\n")
+                        print("\nRestarting......")
 
-                    r = requests.get('http://localhost:8000/playresult')
-                    print(f"\nStatus Code: {r.status_code} --  {r.text} \n")
+
+
+# When the 2nd player submitts they recive the message from the server. the first player needs to be alerted.
+#   Solution 1: loop player 1 in limbo untill player 2 submits ans.
+
+#   Solution 2: Server sends message to all players....(idk why its not)
+
+
+
 
                 elif (int(choice) == 3):
                     payload = {"reset" : 'True'}
